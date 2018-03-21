66285488724c618141667d55ab5d840124db32a9
kpdrozag <kevin.andres01@hotmail.com>
2018-03-21 18:39:53 -0500

    gg

--- barberia/main.py
+++ barberia/main.py
@@ -7,11 +7,10 @@ class Client:
     arrival_t = 0
     service_t = 0
 
-
-
     def __init__(self, at, st):
         self.arrival_t = at
         self.service_t = st
+
 
 class Barber:
     busy = None
@@ -40,19 +39,60 @@ def service_time():
 
 def simulacion():
     hour = 0
+    clients = []  # lista de clientes que llegan
+    number_clients = arrival_clients_hour(True)  # lista de num de clientes x hora
+    for cls in number_clients:
+        for cl in range(cls):
+            clients.append(Client(hour, service_time()))
+        hour += 1
+
+    asientos = queue.Queue(5)  # cola con 5 asientos
+    barber = Barber()  # barbero que esta o no ocupado
+    barber.set_busy(False)  # el barbero no esta ocupado al principio
+
+    i = 0
+    while True:
+
+        if number_clients[i] > 0 or (number_clients == 0 and not asientos.empty()):
+
+
+        minuto = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def simulacion():
+    hour = 0
     clientes = [] # lista de clientes que llegan
     number_clients = arrival_clients_hour(True) # lista de num de clientes x hora
     for cls in number_clients:
         for cl in range(cls):
             clientes.append(Client(hour, service_time()))
         hour += 1
-    """
+
     cc = 1
     print(number_clients)
     for i in clientes:
         print("cliente", cc, "llega en hora ", i.arrival_t, "se atiende en ", i.service_t, "minutos")
         cc += 1
-    """
+
     asientos = queue.Queue(5)  # cola con 5 asientos
     barber = Barber()  # barbero que esta o no ocupado
     barber.set_busy(False)  # el barbero no esta ocupado al principio
@@ -65,3 +105,4 @@ def simulacion():
                 pass
 
 simulacion()
+"""
